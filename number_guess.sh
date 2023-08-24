4225824b56ffd005a07798b2fbb8d35a826007aa
AlexMaderaP <alejandro.maderap@gmail.com>
2023-08-24 16:10:03 -0400

    feat: add read for username

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,27 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 N=$(( RANDOM % 1000 + 1))
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+#Look for username
+#if not found
+#Print welcome message
+#insert new user
+#otherwise print welcome mesage
+
+#Ask for number guess
+#if not a number
+#print ask for number
+#increment times_guessed
+
+#while not guessed
+    #increment times_guessed
+    #if input is higher print lower message
+    #if input is lower print higher message
+    #if not a number
+    #print ask for number
+
+#when guessed print successfull mesage
+#increment games played 
