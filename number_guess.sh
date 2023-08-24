6f84b9770d68108b7ab051c0de3d2a34bf0a2338
AlexMaderaP <alejandro.maderap@gmail.com>
2023-08-24 16:27:38 -0400

    feat: add welcome messages

--- number_guess.sh
+++ number_guess.sh
@@ -7,10 +7,22 @@ echo -e "\nEnter your username:"
 read USERNAME
 
 #Look for username
+USER_INFO=$($PSQL "SELECT username, games_played, best_game FROM usernames WHERE username='$USERNAME'")
 #if not found
-#Print welcome message
-#insert new user
-#otherwise print welcome mesage
+if [[ -z $USER_INFO ]]
+then
+    #Print welcome message
+    echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+    #insert new user
+    GAMES_PLAYED=0
+    BEST_GAME=0
+    INSERT_USER=$($PSQL "INSERT INTO usernames(username,games_played,best_game) VALUES('$USERNAME',$GAMES_PLAYED,$BEST_GAME)")
+else
+    #otherwise print message
+    echo "$USER_INFO" | sed 's/|/ /g' | (read USER GAMES_PLAYED BEST_GAME;
+    echo "Welcome back, $USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.")
+fi
+
 
 #Ask for number guess
 #if not a number
